6dd477fbdb5c36814349dfd4fd41be5f250b446e
jukiaoliveira <jurafaoliveira@icloud.com>
2025-05-27 21:37:47 -0300

    Virtual phone final

--- virtual-phone/main.js
+++ virtual-phone/main.js
@@ -1,26 +1,25 @@
 const display = document.querySelector('input[type=tel]');
-
 const teclas = document.querySelectorAll('input[type=button]');
+const limpar = document.querySelector('.icon');
 
-// tarefa: criar uma condição para travar e não passar de 14 digitos
-function valorTeclasToDisplay (i) {
-    display.value += teclas[i].value;
+function valorTeclasToDisplay(i) {
+    if (display.value.length <= 14) { 
+        display.value += teclas[i].value;
+    } else {
+        alert("PARA DE DIGITAR PORRA!!!!");
+    }
 }
 
-for(let i = 0; i < teclas.length; i++) {
+// Associa o evento de clique a cada tecla
+for (let i = 0; i < teclas.length; i++) {
     teclas[i].onclick = () => {
         valorTeclasToDisplay(i);
     }
 }
 
-teclas[i].onclick = () => {
-    valorTeclasToDisplay(i);
-}
-
-const limpar = document.querySelector('.icon');
-
+// Evento para limpar o display
 limpar.onclick = () => {
-    const opcao = confirm("Deseja limpar o campo?")
+    const opcao = confirm("SEU BURRO, DESEJA LIMPAR O DISPLAY?, TEM CERTEZA?");
     if (opcao) {
         display.value = '';
     }
